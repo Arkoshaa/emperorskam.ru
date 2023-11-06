e3be22ca5d4c182892de381b06a24a345c192a4d
Arkoshaa <150061537+Arkoshaa@users.noreply.github.com>
2023-11-06 19:39:02 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const clickButton = document.querySelector("button")
+let a = 10
+let b = 10
+clickButton.addEventListener("click", () => {
+    while (a == b) {
+        console.log("8438975")
+        while (a == b) {
+            console.log("8438975")
+            while (a == b) {
+                console.log("8438975")
+                while (a == b) {
+                    console.log("8438975")
+                    while (a == b) {
+                        console.log("8438975")
+                    }
+                }
+            }
+        }
+    }
+}, false)  (изменено)
+
